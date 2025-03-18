24e37ff81b7c9dd03cc3794b237aa11c55fed009
Sara Vieira <sara946vieira@hotmail.com>
2025-03-18 20:06:43 -0300

    atualizar função sortearAmigo, não sortear próprio nome ou repetido

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,28 +1,24 @@
-//Criando uma lista para armazenar os nomes
 let amigos = [];
 
-//Função para adicionar nomes de amigos
+
 function adicionarAmigo(){
     let novoNome = document.querySelector('input').value;
-    //Se não inserir um nome exibe mensagem de erro
+
     if (!novoNome){
         alert('Por favor inserir um nome!!');
         return;
     }
 
-    //Adiciona nomes na lista
     amigos.push(novoNome);
-    limparCampo();
     atualizarLista();
+    limparCampo();
 }
 
-//Função para limpar o campo cada vez que adicionado um nome
 function limparCampo(){
     novoNome = document.querySelector('input');
     novoNome.value = "";
 }
 
-//Função para atualizar a lista de amigos no HTML
 function atualizarLista(){
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
@@ -34,31 +30,40 @@ function atualizarLista(){
     } 
 }
 
-//Função para sortear um nome
 function sortearAmigo(){
-    //Variável que indicará o indice da lista
-    let indiceSorteado = Math.floor(Math.random() * amigos.length);
-    //Variavel que armazenará o nome indicado pelo indice sorteado acima
-    let nomeAleatorio = amigos[indiceSorteado];
+    let meuNome = prompt("Digite seu nome:");
+    let indiceSorteado;
+    let nomeAleatorio;
 
-    //Se a lista estiver vazia exibe mensagem de erro, senão sorteia um nome
+    do {
+    indiceSorteado = Math.floor(Math.random() * amigos.length);
+    nomeAleatorio = amigos[indiceSorteado];
+    } while (nomeAleatorio === meuNome);
+
     if (amigos.length === 0){
         alert('Não há nomes para sortear!!')
     } else {
+        amigos.splice(indiceSorteado, 1);
         limparLista();
         document.getElementById('resultado').textContent = 'O amigo secreto sorteado é ' + nomeAleatorio;
     }
 }
 
-//Função para limpar a lista da tela para exibir somente a mensagem de nome sorteado
 function limparLista(){
     let lista = document.getElementById('listaAmigos');
     lista.style.display = 'none';
 }
 
-//Ao clicar no enter ele fará a mesma função que o botão 'Adicionar'
 document.querySelector('input').addEventListener ('keydown', function(event){
     if (event.key === 'Enter'){
         adicionarAmigo();
     }
 })
+
+function verificarNomesSorteados(){
+    for (let i = 0; i < nomesSorteados.length; i++) {
+        if (nomesSorteados[i] === nomeAleatorio) {
+            
+        }
+    }
+}
